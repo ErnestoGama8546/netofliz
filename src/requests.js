33a94127130e7d30cf4d1469c4b3e31896c73a9a
Ernesto Gama <ernestogama8546@gmail.com>
2022-02-01 17:49:11 -0500

    NetoFliz practice project 1st commit

---
+++ src/requests.js
@@ -0,0 +1,9 @@
+const API_KEY = "AIzaSyCyrLvcW2qTuUj4tgb-NYV14C6paQ-GXoE";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    
+
+}
+
+export default requests;
