dee73a3df9e58f9946f4ba62a497fa918907d0f2
Ernesto <ernestogama8546@gmail.com>
2022-02-04 21:33:52 -0500

    added row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './row';
 
 function App() {
   return (
     <div className="App">
       <h1>King NetoFliz </h1>
+      <Row title="NETOFLIZ ORIGINALS" />
+      <Row title="Tendencias"/>
+
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Row({ title }) {
+    return(
+        <div>
+            {/* title */}
+            
+            {/* container -> posters */}
+
+        </div>
+    )
+    
+}
+
+export default Row
