3a82b6b2a111559a57353168fb5984b3b730165a
Ernesto <ernestogama8546@gmail.com>
2022-02-09 21:06:40 -0500

    added css style

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import requests from './requests.js';
 function App() {
   return (
     <div className="App">
-      <h1>King NetoFliz </h1>
-      <Row title="NETOFLIZ ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <h1>NetoFliz </h1>
+      <Row title="NETOFLIZ ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow />
       <Row title="Tendencias" fetchUrl ={requests.fetchTrending}/>
+      <Row title="Pagaron por salir aqui" fetchUrl ={requests.fetchTopRated}/>
       <Row title="Horror" fetchUrl ={requests.fetchHorrorMovies}/>
       <Row title="Action" fetchUrl ={requests.fetchActionMovies}/>
-      <Row title="Pagaron por salir aqui" fetchUrl ={requests.fetchTopRated}/>
+      <Row title="Romance" fetchUrl ={requests.fetchRomanceMovies}/>
+      <Row title="Comedia" fetchUrl ={requests.fetchComedyMovies}/>
+      <Row title="Documentales" fetchUrl ={requests.fetchDocumentaries}/>
 
     </div>
   );
