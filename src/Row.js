9b0af66d90fbedd63c2ffd1a65361e53ac3ddb54
Ernesto <ernestogama8546@gmail.com>
2022-02-09 19:55:52 -0500

    images now rendering

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react"
-import axios from './axios'
+import React, { useState, useEffect } from "react";
+import axios from './axios';
+/*import "./Row.css";*/
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies,setMovies] = useState ([]);
+    const [movies, setMovies] = useState ([]);
 
     // A snippet of code which runs based on a specific condition
     useEffect(()=> {
@@ -16,6 +17,7 @@ function Row({ title, fetchUrl }) {
         }
         fetchData();
     }, [fetchUrl]);
+    
 
     return(
         <div className="row">
@@ -24,13 +26,18 @@ function Row({ title, fetchUrl }) {
             <div className="row_posters">
               {/* several row posters */}
               
-              {movies.map(movies => (
-                  <img src={`${base_url}${movies.poster_path}`} alt={movies.name}/>
+              {movies.map((movie) => (
+                  <img 
+                    key={movie.id}
+                    className="row_poster"
+                    src={`${base_url}${movie.poster_path}`}
+                    alt={movie.name}
+                  />
               ))}
             </div>
         </div>
-    )
+    );
     
 }
 
-export default Row
+export default Row;
